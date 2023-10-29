18bef4b2bf6029e3c75c81709ca303db58e6619f
Sriharsha <sriharshasaligrama@gmail.com>
2023-10-29 23:51:18 +0530

    Auth not working

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,24 @@
-export default function Page() {
-    return <p>Customers Page</p>;
+import { Suspense } from "react";
+import Table from '@/app/ui/customers/table';
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
+import { fetchFilteredCustomers } from "@/app/lib/data";
+
+export default async function Page({
+    searchParams,
+}: {
+    searchParams?: {
+        query?: string;
+        page?: string;
+    };
+}) {
+    const query = searchParams?.query || '';
+    const filteredCustomers = await fetchFilteredCustomers(query);
+
+    return (
+        <div className="w-full">
+            <Suspense key={query} fallback={<InvoicesTableSkeleton />}>
+                <Table customers={filteredCustomers} />
+            </Suspense>
+        </div>
+    )
 }
